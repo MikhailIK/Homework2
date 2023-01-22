0040c04f82079959e288b458988789e3350948eb
MikhailIK <raxmet777@yandex.ru>
2023-01-22 22:09:55 +0300

    adden new files

---
+++ script.js
@@ -0,0 +1,83 @@
+//task 1
+let a;
+a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//task 2
+
+let year = 2007;
+alert(year);
+
+//task 3
+
+let Name = "Брендан";
+alert(Name);
+
+//task 4
+
+let x = 10;
+let y = 2;
+alert("Сумма: " + (x+y) + ". Разность: " + (x-y) + ". Произведение: " + x * y + ". Частное: " + x / y + ".");
+
+//task 5
+
+let result = Math.pow(2,5);
+alert(result);
+
+//task 6
+
+a = 9;
+let b = 2;
+let module = a%b;
+alert(module);
+
+//task 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+//task 8
+
+let age;
+age = prompt("Сколько вам лет?")
+alert(age);
+
+//task 9
+
+//9.0
+const user = {
+    name: "Bob",
+    age: 32,
+    isAdmin: false
+}
+
+//9.1
+
+user['city of residence'];
+
+//9.2
+
+user.age = 35;
+
+//9.3
+
+delete['city of residence'];
+
+//9.4
+
+let info = prompt("Что вы хотите узнать о пользователе?", "name");
+
+alert(user[info]);
+
+//task 10
+
+userName = prompt("Как вас зовут?");
+alert("Привет " + userName + "!");
